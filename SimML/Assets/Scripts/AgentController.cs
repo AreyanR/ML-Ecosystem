82cef64de4af565e21adadbc85d185f664ba4321
Areyan Rastawan <areyanr@hotmail.com>
2024-07-16 23:55:14 -0700

    Added Vision for ML/ Env score color

--- SimML/Assets/Scripts/AgentController.cs
+++ SimML/Assets/Scripts/AgentController.cs
@@ -8,24 +8,72 @@ using Unity.MLAgents.Sensors;
 public class AgentController : Agent
 {
 
-    
-    [SerializeField] private float moveSpeed = 4f;
+    //Pellet variables
+    public int pelletCount;
+    public GameObject food;
+    [SerializeField] private List<GameObject> spawnedPelletList = new List<GameObject>();
     [SerializeField] private Transform target;
 
+
+//Agent variables
+    [SerializeField] private float moveSpeed = 4f;
     private Rigidbody rb;
+
+
+    // env variables
+    [SerializeField] private Transform environmentLocation;
+    Material envMaterial;
+    public GameObject env;
+
 
     public override void Initialize()
     {
         rb = GetComponent<Rigidbody>();
+        envMaterial = env.GetComponent<Renderer>().material;
     }
 
     public override void OnEpisodeBegin()
     {
+        //Agent
         transform.localPosition = new Vector3(Random.Range(-4f,4f),0.31f,Random.Range(-4f,4f));
 
-        target.localPosition = new Vector3(Random.Range(-4f,4f),0.31f,Random.Range(-4f,4f));
+        //target.localPosition = new Vector3(Random.Range(-4f,4f),0.31f,Random.Range(-4f,4f));
+
+        //pellet
+        CreatePellet();
         
         
+    }
+
+
+    private void CreatePellet()
+    {
+        if(spawnedPelletList.Count != 0)
+        {
+            RemovePellet(spawnedPelletList);
+        }
+
+        for (int i = 0; i < pelletCount; i++)
+        {
+            GameObject newPellet = Instantiate(food);
+            //make pellet child of env
+            newPellet.transform.parent = environmentLocation;
+            //Give random spawn location
+            Vector3 pelletLocation = new Vector3(Random.Range(-4f,4f),0.31f,Random.Range(-4f,4f));
+            //Spawn in new location
+            newPellet.transform.localPosition = pelletLocation;
+            //Add to list
+            spawnedPelletList.Add(newPellet);
+        }
+    }
+
+    private void RemovePellet(List<GameObject> spawnedPelletList)
+    {
+        foreach (GameObject i in spawnedPelletList)
+        {
+            Destroy(i.gameObject);
+        }
+        spawnedPelletList.Clear();
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -64,13 +112,24 @@ public class AgentController : Agent
     {
         if(other.gameObject.tag == "Pellet")
         {
-            AddReward(5f);
-            EndEpisode();
+            //Remove from list
+            spawnedPelletList.Remove(other.gameObject);
+            Destroy(other.gameObject);
+            AddReward(10f);
+            if(spawnedPelletList.Count == 0)
+            {
+                envMaterial.color = Color.green;
+                RemovePellet(spawnedPelletList);
+                AddReward(5f);
+                EndEpisode();
+            }
         }
 
         if(other.gameObject.tag == "Wall")
         {
-            AddReward(-1f);
+            envMaterial.color = Color.red;
+            RemovePellet(spawnedPelletList);
+            AddReward(-15f);
             EndEpisode();
         }
     }
